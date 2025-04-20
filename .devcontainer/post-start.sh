2432e2534333e711e4471bb9fe88ce293aa5429a
Chirag Panwar <125826529+ChiR24@users.noreply.github.com>
2025-04-20 16:21:23 +0530

    fix

--- .devcontainer/post-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running post-start script..."
-
-# Start MongoDB service
-sudo service mongod start || echo "Failed to start MongoDB service"
-
-echo "Post-start script completed." 
